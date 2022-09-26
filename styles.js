9e6b28536705459203deda0edd2130ae3220cedb
moralsports@gmail.com <moralsports@gmail.com>
2022-09-26 19:36:16 -0400

    first-Commit

---
+++ styles.js
@@ -0,0 +1,13 @@
+function myRandomRgb() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`;
+}
+const letters = document.querySelectorAll(".letter");
+
+setInterval(() => {
+  for (let letter of letters) {
+    letter.style.color = myRandomRgb();
+  }
+}, 500);
